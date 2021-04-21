1e5e40b092c099ea6a6aa5bc073c37651a7feed0
yogurtfinance <yogurtfinance@gmail.com>
2021-04-21 23:13:42 +0000

    first commit

---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,190 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.pancakeswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.pancakeswap.finance/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://pancakeswap.info",
+      },
+      {
+        label: "Tokens",
+        href: "https://pancakeswap.info/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://pancakeswap.info/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://pancakeswap.info/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      // {
+      //   label: "Voting",
+      //   href: "https://voting.pancakeswap.finance",
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/yogurtfinance/",
+      },
+      {
+        label: "Docs",
+        href: "https://yogurtfinance.gitbook.io/yogurt-finance/",
+      },
+      {
+        label: "Blog",
+        href: "https://yogurtfinance.medium.com/",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/yogurtfinance",
+      },
+      // {
+      //   label: "Bahasa Indonesia",
+      //   href: "https://t.me/PancakeSwapIndonesia",
+      // },
+      {
+        label: "中文",
+        href: "https://t.me/yogurtfinancechinese",
+      },
+      // {
+      //   label: "Tiếng Việt",
+      //   href: "https://t.me/PancakeSwapVN",
+      // },
+      {
+        label: "Italiano",
+        href: "https://t.me/yogurtfinanceitalian",
+      },
+      {
+        label: "русский",
+        href: "https://t.me/yogurtfinancerussian",
+      },
+      {
+        label: "Türkiye",
+        href: "https://t.me/yogurtFinanceTurkey",
+      },
+      // {
+      //   label: "Português",
+      //   href: "https://t.me/PancakeSwapPortuguese",
+      // },
+      {
+        label: "Español",
+        href: "https://t.me/yogurtfinancespanish",
+      },
+      {
+        label: "日本語",
+        href: "https://t.me/yogurtfinancejapanese",
+      },
+      {
+        label: "Français",
+        href: "https://t.me/yogurtfinancefrench",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/yogurtfinanceann",
+      },
+      {
+        label: "Price Bot",
+        href: "https://t.me/BinanceRocketEGG",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/yogurtfinance",
+  },
+  {
+    label: "Reddit",
+    icon: "RedditIcon",
+    href: "https://www.reddit.com/r/YogurtFinanceofficial",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
