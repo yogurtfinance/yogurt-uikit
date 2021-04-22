fdbfc60b1a36636333e30c6c753649359a7150f8
yogurtfinance <admin@yogurtfinance.com>
2021-04-22 03:36:29 +0300

    aa

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2275,7 +2275,7 @@ var socials = [
     {
         label: "Reddit",
         icon: "RedditIcon",
-        href: "https://www.reddit.com/r/YogurtFinanceofficial",
+        href: "https://www.reddit.com/user/YogurtFinance",
     },
 ];
 var MENU_HEIGHT = 64;
